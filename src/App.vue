5a9f5287dd4bf3f32e2ba55a29944e836061031d
pubr-wthan <120130146+pubr-wthan@users.noreply.github.com>
2023-08-11 10:09:44 +0700

    info lab02

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink class="link" to="/">Home</RouterLink>
+      <RouterLink class="link" to="/about">About</RouterLink>
+      <RouterLink class="link" to="/organizer">Organizer</RouterLink>
+      <RouterLink class="link" to="/student">Student</RouterLink>
+    </nav>
+  </header>
+  <RouterView />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.link {
+  margin-right: 10px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+h4 {
+  font-size: 20px;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 70vh;
+    display: flex;
+    justify-content: center; /* Center horizontally */
+    align-items: center; /* Center vertically */
+  }
+}
+</style>
